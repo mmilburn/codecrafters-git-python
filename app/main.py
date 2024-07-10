cc06c85df921b9e8311ebe90088812ed936c1664
Mark Milburn <mark.a.milburn@gmail.com>
2024-07-10 14:56:05 -0600

    Implement code to pass "Read a tree object #kp1"

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 import hashlib
+import io
 import os
 import sys
 import zlib
@@ -28,20 +29,26 @@ def create_blob_string(content):
     return f"blob {size}\0{content}".encode("utf-8")
 
 
-def extract_content(blob_string):
-    # Find the position of the null character '\0'
-    null_index = blob_string.find('\0')
+def extract_content(object_string: bytes):
+    # Find the position of the first null character '\0'
+    null_index = object_string.find(b'\0')
 
     if null_index == -1:
-        raise ValueError("Invalid blob format: missing null character")
-
+        raise ValueError("Invalid blob or tree format: missing null character")
     # Extract <size> and <content>
-    size_str = blob_string[5:null_index]  # Start after "blob " and end before '\0'
-    content_start_index = null_index + 1
-    content_length = int(size_str)
-    content = blob_string[content_start_index:content_start_index + content_length]
+    size_str = object_string[5:null_index]  # Start after "blob " or "tree " and end before '\0'
+    if object_string.startswith(b"blob") or object_string.startswith(b"tree"):
+        content_start_index = null_index + 1
+        content_length = int(size_str)
+        content = object_string[content_start_index:content_start_index + content_length]
+        return content
+    return None
 
-    return content
+
+def object_path(sha: str) -> str:
+    hash_dir = sha[:2]
+    object_file = sha[2:]
+    return os.path.join(GIT_OBJECTS_PATH, hash_dir, object_file)
 
 
 def main():
@@ -55,13 +62,11 @@ def main():
         print("Initialized git directory")
     elif command == "cat-file":
         sha = sys.argv[-1]
-        hash_dir = sha[:2]
-        object_file = sha[2:]
-        object_path = os.path.join(GIT_OBJECTS_PATH, hash_dir, object_file)
-        if os.path.isfile(object_path):
+        o_path = object_path(sha)
+        if os.path.isfile(o_path):
             try:
-                with open(object_path, "rb") as f:
-                    content = extract_content(zlib.decompress(f.read()).decode("utf-8"))
+                with open(o_path, "rb") as f:
+                    content = extract_content(zlib.decompress(f.read())).decode("utf-8")
                     print(content, end="")
             except Exception as e:
                 print(f"Exception occurred: {type(e).__name__}: {e}", file=sys.stderr)
@@ -74,13 +79,37 @@ def main():
         object_dir = os.path.join(GIT_OBJECTS_PATH, hash_dir)
         if not os.path.exists(object_dir):
             os.mkdir(object_dir)
-        object_path = os.path.join(object_dir, object_file)
+        o_path = os.path.join(object_dir, object_file)
         try:
-            with open(object_path, "wb") as f:
+            with open(o_path, "wb") as f:
                 f.write(zlib.compress(blob))
         except Exception as e:
             print(f"Exception occurred: {type(e).__name__}: {e}", file=sys.stderr)
         print(digest)
+    elif command == "ls-tree":
+        sha = sys.argv[-1]
+        name_only = sys.argv[-2]
+        o_path = object_path(sha)
+        if os.path.isfile(o_path):
+            try:
+                with open(o_path, "rb") as f:
+                    content = extract_content(zlib.decompress(f.read()))
+                    #print("converting content to stream")
+                    null_index = content.find(b"\0")
+                    prev = 0
+                    while null_index != -1:
+                        mode, name = content[prev:null_index].split(b" ")
+                        mode = mode.decode("utf-8")
+                        name = name.decode("utf-8")
+                        digest = content[null_index + 1: null_index + 21]
+                        prev = null_index + 21
+                        if name_only == "--name-only":
+                            print(name)
+                        else:
+                            print(f"{mode} {name} {digest.hex()}")
+                        null_index = content.find(b"\0", null_index + 1)
+            except Exception as e:
+                print(f"Exception occurred: {type(e).__name__}: {e}", file=sys.stderr)
     else:
         raise RuntimeError(f"Unknown command #{command}")
 
