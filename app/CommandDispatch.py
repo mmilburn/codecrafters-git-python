a639683c0ba92b3711f08e9c378a8e27c99342bc
Mark Milburn <mark.a.milburn@gmail.com>
2025-01-05 15:34:22 -0700

    codecrafters submit [skip ci]

--- app/CommandDispatch.py
+++ app/CommandDispatch.py
@@ -1,7 +1,8 @@
 import inspect
+import struct
 import time
 import urllib.request
-from pathlib import Path
+from typing import cast
 
 from app.util import *
 
@@ -14,8 +15,8 @@ class CommandDispatch:
 
     def _build_dispatch_table(self):
         for name, method in inspect.getmembers(self, predicate=inspect.ismethod):
-            if name.startswith('cmd_'):
-                cmd_name = name[4:]  # Remove prefix
+            if name.startswith("cmd_"):
+                cmd_name = name.replace("cmd_", "")  # Remove prefix
                 cmd_name = cmd_name.replace("_", "-")
                 self.dispatch_dict[cmd_name] = method
 
@@ -48,55 +49,45 @@ class CommandDispatch:
                 print(f"Exception occurred: {type(e).__name__}: {e}", file=sys.stderr)
 
     def cmd_hash_object(self, *args, **kwargs):
-        return write_blob(read_file(args[-1][-1])).hexdigest() + "\n"
+        return write_object("blob", Path("."), Path(args[-1][-1]).read_bytes()).hexdigest() + "\n"
 
+    # Courtesy ggzor -- https://app.codecrafters.io/users/ggzor
     def cmd_ls_tree(self, *args, **kwargs):
-        sha = args[-1][-1]
+        tree_sha = args[-1][-1]
         name_only = args[-1][-2]
-        o_path = object_path(sha)
-        if os.path.isfile(o_path):
-            try:
-                with open(o_path, "rb") as f:
-                    content = extract_content(zlib.decompress(f.read()))
-                    #print("converting content to stream")
-                    null_index = content.find(b"\0")
-                    prev = 0
-                    result = ""
-                    while null_index != -1:
-                        print(content[prev:null_index], file=sys.stderr)
-                        mode, name = content[prev:null_index].decode("utf-8").split(" ")
-                        digest = content[null_index + 1: null_index + 21]
-                        prev = null_index + 21
-                        if name_only == "--name-only":
-                            result += name + "\n"
-                        else:
-                            result += f"{mode} {name} {digest.hex()}\n"
-                        null_index = content.find(b"\0", null_index + 1)
-                    return result
-            except Exception as e:
-                print(f"Exception occurred: {type(e).__name__}: {e}", file=sys.stderr)
+        entries = []
+        _, contents = read_object(Path("."), tree_sha)
+        while contents:
+            mode, contents = contents.split(b" ", 1)
+            name, contents = contents.split(b"\0", 1)
+            sha = contents[:20]
+            contents = contents[20:]
+            entries.append((mode.decode(), name.decode(), sha.hex()))
+        if name_only == "--name-only":
+            return "\n".join([entry[1] for entry in entries]) + "\n"
+        return "\n".join([f"{entry[0]} {entry[1]} {entry[2]}" for entry in entries]) + "\n"
 
     def cmd_write_tree(self, *args, **kwargs):
-        return write_tree(create_tree(os.getcwd())).hexdigest() + "\n"
+        return write_object("tree", Path("."), create_tree(os.getcwd())).hexdigest() + "\n"
 
     def cmd_commit_tree(self, *args, **kwargs):
         tree_sha = args[-1][0]
         arg_list = args[-1]
         commit_sha, message = "", ""
         for index in range(len(arg_list)):
-            if  arg_list[index] == "-p":
+            if arg_list[index] == "-p":
                 commit_sha = arg_list[index + 1]
             elif arg_list[index] == "-m":
-                message = arg_list[index + 1].replace("\"","").replace("'", "")
+                message = arg_list[index + 1].replace("\"", "").replace("'", "")
         print(f"tree_sha: {tree_sha} commit_sha: {commit_sha} message: {message}", file=sys.stderr)
         # Courtesy Ben Hoyt https://benhoyt.com/writings/pygit/#committing
         timestamp = int(time.mktime(time.localtime()))
         utc_offset = -time.timezone
         author_time = "{} {}{:02}{:02}".format(
-                timestamp,
-                '+' if utc_offset > 0 else '-',
-                abs(utc_offset) // 3600,
-                (abs(utc_offset) // 60) % 60)
+            timestamp,
+            '+' if utc_offset > 0 else '-',
+            abs(utc_offset) // 3600,
+            (abs(utc_offset) // 60) % 60)
         content = ["tree " + tree_sha]
         # parent commit
         if commit_sha:
@@ -106,4 +97,94 @@ class CommandDispatch:
         content.append("")
         content.append(message)
         content.append("")
-        return write_commit("\n".join(content).encode()).hexdigest() + "\n"
+        return write_object("commit", Path("."), "\n".join(content).encode()).hexdigest() + "\n"
+
+    # Courtesy ggzor -- https://app.codecrafters.io/users/ggzor
+    def cmd_clone(self, *args, **kwargs):
+        """ The fetch command underlies two other commonly used commands. clone is used to set up a
+        new repository that’s a copy of an existing one, and this can be accomplished using the init,
+        remote, fetch and reset commands. --Building Git
+        `init` is already implemented.
+        I don't think we need `remote add` since our clone operation is a one-shot deal.
+        Likewise, I don't think a reset is necessary.
+        """
+        clone_url = args[-1][-2]
+        parent = Path(args[-1][-1])
+        print(f"clone_url: {clone_url} clone_dir: {parent}", file=sys.stderr)
+        self.cmd_init(*args)
+        req = urllib.request.Request(f"{clone_url}/info/refs?service=git-upload-pack")
+        with urllib.request.urlopen(req) as f:
+            refs = {
+                chunk[1].decode(): chunk[0].decode()
+                for chunk0 in cast(bytes, f.read()).split(b"\n")
+                if (chunk1 := chunk0[4:])
+                   and not chunk1.startswith(b"#")
+                   and (chunk2 := chunk1.split(b"\0")[0])
+                   and (chunk := (chunk2[4:] if chunk2.endswith(b"HEAD") else chunk2).split(b" "))
+            }
+        # create ref entries
+        for name, sha in refs.items():
+            Path(parent / ".git" / name).write_text(f"{sha}\n")
+
+        body = (b"0011command=fetch0001000fno-progress"
+                + b"".join(b"0032want " + ref.encode() + b"\n" for ref in refs.values())
+                + b"0009done\n0000")
+        req = urllib.request.Request(f"{clone_url}/git-upload-pack", data=body, headers={"Git-Protocol": "version=2"})
+        with urllib.request.urlopen(req) as f:
+            pack_bytes = cast(bytes, f.read())
+        pack_lines = []
+        while pack_bytes:
+            line_len = int(pack_bytes[:4], 16)
+            if line_len == 0:
+                break
+            pack_lines.append(pack_bytes[4:line_len])
+            pack_bytes = pack_bytes[line_len:]
+        pack_file = b"".join(l[1:] for l in pack_lines[1:])
+        pack_file = pack_file[8:]  # strip header, version
+        n_objs, *_ = struct.unpack("!I", pack_file[:4])
+        pack_file = pack_file[4:]
+        for _ in range(n_objs):
+            kind, _, pack_file = next_size_type(pack_file)
+            match kind:
+                case "commit" | "tree" | "blob" | "tag":
+                    dec = zlib.decompressobj()
+                    content = dec.decompress(pack_file)
+                    pack_file = dec.unused_data
+                    write_object(kind, parent, content)
+                case "ref_delta":
+                    obj = pack_file[:20].hex()
+                    pack_file = pack_file[20:]
+                    dec = zlib.decompressobj()
+                    content = dec.decompress(pack_file)
+                    pack_file = dec.unused_data
+                    target_content = b""
+                    base_kind, base_content = read_object(parent, obj)
+                    _, content = next_size(content)
+                    _, content = next_size(content)
+                    while content:
+                        is_copy = content[0] & 0b_1000_0000
+                        if is_copy:
+                            data_ptr = 1
+                            offset = 0
+                            size = 0
+                            for i in range(0, 4):
+                                if content[0] & (1 << i):
+                                    offset |= content[data_ptr] << (i * 8)
+                                    data_ptr += 1
+                            for i in range(0, 3):
+                                if content[0] & (1 << (4 + i)):
+                                    size |= content[data_ptr] << (i * 8)
+                                    data_ptr += 1
+                            content = content[data_ptr:]
+                            target_content += base_content[offset: offset + size]
+                        else:
+                            size = content[0]
+                            addition = content[1: size + 1]
+                            content = content[size + 1:]
+                            target_content += addition
+                    write_object(base_kind, parent, target_content)
+                case _:
+                    raise RuntimeError("Not implemented")
+        _, commit = read_object(parent, refs["HEAD"])
+        tree_sha = commit[5: 40 + 5].decode()
+        render_tree(parent, parent, tree_sha)
--- app/util.py
+++ app/util.py
@@ -1,18 +1,18 @@
-from genericpath import isdir
-import os
-from posix import listdir
-import sys
 import hashlib
+import os
+import sys
 import zlib
+from pathlib import Path
 
 GIT_DIR = ".git"
 GIT_OBJECTS_PATH = os.path.join(GIT_DIR, "objects")
-GIT_REFS_PATH = os.path.join(GIT_DIR, "refs")
+
 
 def object_path(sha: str) -> str:
     hash_dir = sha[:2]
     object_file = sha[2:]
     return os.path.join(GIT_OBJECTS_PATH, hash_dir, object_file)
+
 
 def extract_content(object_string: bytes):
     # Find the position of the first null character '\0'
@@ -27,38 +27,47 @@ def extract_content(object_string: bytes):
         content_length = int(size_str)
         content = object_string[content_start_index:content_start_index + content_length]
         return content
-    return b""
 
-def _create_entry(type, content):
-    if isinstance(content, str):
-        content = content.encode("utf-8")
-    size = len(content)
-    return f"{type} {size}\0".encode("utf-8") + content
 
-def _write_object(object):
-    raw_digest = hashlib.sha1(object)
+def read_object(parent, sha):
+    directory = sha[:2]
+    obj = sha[2:]
+    p = (parent / ".git" / "objects" / directory / obj)
+    byte_stream = p.read_bytes()
+    header, content = zlib.decompress(byte_stream).split(b"\0", 1)
+    kind, _ = header.split(b" ")
+    return kind.decode(), content
+
+
+def write_object(obj_type: str, parent: Path, content: bytes):
+    content = f"{obj_type} {len(content)}\0".encode() + content
+    raw_digest = hashlib.sha1(content)
     digest = raw_digest.hexdigest()
     hash_dir = digest[:2]
     object_file = digest[2:]
-    object_dir = os.path.join(GIT_OBJECTS_PATH, hash_dir)
-    if not os.path.exists(object_dir):
-        os.mkdir(object_dir)
-    o_path = os.path.join(object_dir, object_file)
-    try:
-        with open(o_path, "wb") as f:
-            f.write(zlib.compress(object))
-    except Exception as e:
-        print(f"Exception occurred: {type(e).__name__}: {e}", file=sys.stderr)
+    p = (parent / ".git" / "objects" / hash_dir / object_file)
+    p.parent.mkdir(parents=True, exist_ok=True)
+    p.write_bytes(zlib.compress(content))
     return raw_digest
 
-def write_blob(content):
-    return _write_object(_create_entry("blob", content))
 
-def write_tree(content):
-    return _write_object(_create_entry("tree", content))
+def render_tree(parent: Path, dir: Path, sha: str):
+    dir.mkdir(parents=True, exist_ok=True)
+    _, tree = read_object(parent, sha)
+    while tree:
+        mode, tree = tree.split(b" ", 1)
+        name, tree = tree.split(b"\0", 1)
+        sha = tree[:20].hex()
+        tree = tree[20:]
+        match mode:
+            case b"40000":
+                render_tree(parent, dir / name.decode(), sha)
+            case b"100644":
+                _, content = read_object(parent, sha)
+                Path(dir / name.decode()).write_bytes(content)
+            case _:
+                raise RuntimeError("Not implemented")
 
-def write_commit(content):
-    return _write_object(_create_entry("commit", content))
 
 def read_file(file_path):
     content = None
@@ -72,11 +81,14 @@ def read_file(file_path):
         print(f"File not found: {file_path}", file=sys.stderr)
     return content
 
+
 def is_symlink(file_path):
     return os.path.islink(file_path)
 
+
 def is_executable(file_path):
     return os.path.isfile(file_path) and os.access(file_path, os.X_OK)
+
 
 def get_mode(file_path):
     if os.path.isdir(file_path):
@@ -90,6 +102,7 @@ def get_mode(file_path):
         else:
             return "100644"
 
+
 def create_tree(path):
     entries = {}
     listings = filter(lambda x: x != GIT_DIR, os.listdir(path))
@@ -97,7 +110,47 @@ def create_tree(path):
     for entry in listings:
         entry_path = os.path.join(path, entry)
         if os.path.isfile(entry_path):
-            entries[entry] = f"{get_mode(entry_path)} {entry}\0".encode("utf-8") + write_blob(read_file(entry_path)).digest()
+            entries[entry] = f"{get_mode(entry_path)} {entry}\0".encode("utf-8") + write_object("blob", Path("."), Path(
+                str(entry_path)).read_bytes()).digest()
         else:
-            entries[entry] = f"{get_mode(entry_path)} {entry}\0".encode("utf-8") + write_tree(create_tree(entry_path)).digest()
+            entries[entry] = f"{get_mode(entry_path)} {entry}\0".encode("utf-8") + write_object("tree", Path("."),
+                                                                                                create_tree(
+                                                                                                    entry_path)).digest()
     return b"".join([entries[key] for key in sorted(entries)])
+
+# All code below courtesy ggzor -- https://app.codecrafters.io/users/ggzor
+def next_size_type(byte_stream):
+    kind = (byte_stream[0] & 0b_0111_0000) >> 4
+    match kind:
+        case 1:
+            kind = "commit"
+        case 2:
+            kind = "tree"
+        case 3:
+            kind = "blob"
+        case 4:
+            kind = "tag"
+        case 6:
+            kind = "ofs_delta"
+        case 7:
+            kind = "ref_delta"
+        case _:
+            kind = "unknown"
+    size = byte_stream[0] & 0b_0000_1111
+    i = 1
+    off = 4
+    while byte_stream[i - 1] & 0b_1000_0000:
+        size += (byte_stream[i] & 0b_0111_1111) << off
+        i += 1
+        off += 7
+    return kind, size, byte_stream[i:]
+
+
+def next_size(byte_stream):
+    size = byte_stream[0] & 0b0111_1111
+    i, off = 1, 7
+    while byte_stream[i - 1] & 0b_1000_0000:
+        size += (byte_stream[i] & 0b_0111_1111) << off
+        off += 7
+        i += 1
+    return size, byte_stream[i:]
